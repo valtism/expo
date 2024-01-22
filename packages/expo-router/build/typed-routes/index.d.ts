fc742413cff651f84a1c7cf4135e6889d20973fd
Mark Lawlor <mwlawlor@gmail.com>
2024-01-22 14:40:28 +1000

    [router] create typed route helpers that use getRoutes()

---
+++ packages/expo-router/build/typed-routes/index.d.ts
@@ -0,0 +1,6 @@
+export declare function getGenerateTypeDeclarationHandler(outputDir: string): ({ filePath, type }: {
+    filePath: string;
+    type: string;
+}) => Promise<void>;
+export declare const generateTypeDeclarations: (outputDir: string) => void;
+//# sourceMappingURL=index.d.ts.map
